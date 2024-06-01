014e13eeffb144236e6c8e2327c0066e2fbe9f95
OSSU <o.ss.u2056@gmail.com>
2024-06-02 08:37:03 +0900

    fix: 사용자 포인트 업데이트 스웨거 예시 수정

--- src/main/java/yteamserver/domain/users/dto/UserPointReq.java
+++ src/main/java/yteamserver/domain/users/dto/UserPointReq.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 @Getter
 public class UserPointReq {
     @Schema(description = "사용자 토큰",
-            example = "odkaj183-ga94-9aj3-1d81-djal194ns93")
+            example = "d6f6d982-cb17-4ce5-aab4-ba16e2e5f1d4")
     private String token;
     @Schema(description = "포인트", example = "100 // 100포인트 증가시 +100, 감소시 -100")
     private Integer points;
