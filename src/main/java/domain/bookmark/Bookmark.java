d9b77673e66153d4ffcf34c8b8838142ff4c6297
OSSU <o.ss.u2056@gmail.com>
2024-06-01 15:18:01 +0900

    feat: 기본 엔티티 세팅

--- src/main/java/domain/bookmark/Bookmark.java
+++ src/main/java/domain/bookmark/Bookmark.java
@@ -1,4 +1,26 @@
 package domain.bookmark;
 
+import domain.store.Store;
+import domain.users.Users;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "bookmark")
 public class Bookmark {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @Column(name = "user_id")
+    private Users users;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @Column(name = "store_id")
+    private Store store;
 }
--- src/main/java/domain/users/Users.java
+++ src/main/java/domain/users/Users.java
@@ -1,4 +1,25 @@
 package domain.users;
 
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "users")
 public class Users {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "profile_url")
+    private String profileUrl;
+
+    @Column(name = "token")
+    private String token;
 }
