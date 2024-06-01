c2c525d6747de054e9d491811c480ec760dcbd3a
OSSU <o.ss.u2056@gmail.com>
2024-06-01 16:24:24 +0900

    feat: 응답 형태 통일

---
+++ src/main/java/global/error/GeneralException.java
@@ -0,0 +1,39 @@
+package global.error;
+
+import lombok.Getter;
+
+@Getter
+public class GeneralException extends RuntimeException {
+
+    private final ErrorCode errorCode;
+
+    public GeneralException() {
+        super(ErrorCode.SERVER_ERROR.getErrorMessage());
+        this.errorCode = ErrorCode.SERVER_ERROR;
+    }
+
+    public GeneralException(String message) {
+        super(ErrorCode.SERVER_ERROR.getErrorMessage(message));
+        this.errorCode = ErrorCode.SERVER_ERROR;
+    }
+
+    public GeneralException(String message, Throwable cause) {
+        super(ErrorCode.SERVER_ERROR.getErrorMessage(message), cause);
+        this.errorCode = ErrorCode.SERVER_ERROR;
+    }
+
+    public GeneralException(Throwable cause) {
+        super(ErrorCode.SERVER_ERROR.getErrorMessage(cause));
+        this.errorCode = ErrorCode.SERVER_ERROR;
+    }
+
+    public GeneralException(ErrorCode errorCode) {
+        super(errorCode.getErrorMessage());
+        this.errorCode = errorCode;
+    }
+
+    public GeneralException(ErrorCode errorCode, Throwable cause) {
+        super(errorCode.getErrorMessage(cause), cause);
+        this.errorCode = errorCode;
+    }
+}
---
+++ src/main/java/global/response/Response.java
@@ -0,0 +1,48 @@
+package global.response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import global.error.ErrorCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+@Getter
+@RequiredArgsConstructor
+public class Response {
+    private final Boolean success;
+    private final Integer code;
+    private final String message;
+    @JsonInclude(Include.NON_NULL)
+    private final Object data;
+
+    // HttpStatus에 대한 응답
+    public static ResponseEntity<Response> of(HttpStatus status) {
+        return new ResponseEntity<>(
+                new Response(true, status.value(), status.getReasonPhrase(), null),
+                status
+        );
+    }
+
+    // ErrorCode에 대한 응답
+    public static ResponseEntity<Response> of(ErrorCode errorCode) {
+        return of(errorCode, null);
+    }
+
+    public static ResponseEntity<Response> of(ErrorCode errorCode, String message) {
+        return new ResponseEntity<>(
+                new Response(false, errorCode.getCode(), errorCode.getErrorMessage(message), null),
+                errorCode.getHttpStatus()
+        );
+    }
+
+    // 데이터에 대한 응답
+    public static ResponseEntity<Response> of(HttpStatus status, Object data) {
+        return new ResponseEntity<>(
+                new Response(true, status.value(), status.getReasonPhrase(), data),
+                status
+        );
+    }
+}
+
