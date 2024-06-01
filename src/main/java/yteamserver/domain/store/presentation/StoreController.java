54d1b13b138a26a3d18af9600b5f6f724781e7be
OSSU <o.ss.u2056@gmail.com>
2024-06-01 21:33:57 +0900

    feat(#6): 요즘 뜨는 셀러 TOP6 구현 API 스웨거 추가

--- src/main/java/yteamserver/domain/store/presentation/StoreController.java
+++ src/main/java/yteamserver/domain/store/presentation/StoreController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import yteamserver.domain.store.application.StoreService;
+import yteamserver.domain.store.dto.GetTopStoresRes;
 import yteamserver.domain.video.dto.CreateVideoReq;
 import yteamserver.global.response.Response;
 
@@ -24,6 +25,12 @@ public class StoreController {
     private final StoreService storeService;
 
     @Operation(summary = "요즘 뜨는 셀러 TOP6", description = "메인 화면에서 숏폼 영상 조회수 상위 6개의 업체를 조회합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "요즘 뜨는 셀러 TOP6 조회 성공",
+                    content = @Content(schema = @Schema(implementation = GetTopStoresRes.class))),
+            @ApiResponse(responseCode = "500", description = "서버 에러 발생",
+                    content = @Content(schema = @Schema(implementation = Response.class)))
+    })
     @GetMapping("/top")
     public ResponseEntity<Response> getTopStore() {
         return Response.of(HttpStatus.OK, storeService.getTopStore());
