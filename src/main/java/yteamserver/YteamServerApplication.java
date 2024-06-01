b1227192cf2a3f0ed967b3cabc12e3f592318236
OSSU <o.ss.u2056@gmail.com>
2024-06-01 13:01:00 +0900

    init project

---
+++ src/main/java/yteamserver/YteamServerApplication.java
@@ -0,0 +1,13 @@
+package yteamserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YteamServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YteamServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/yteamserver/YteamServerApplicationTests.java
@@ -0,0 +1,13 @@
+package yteamserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YteamServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
