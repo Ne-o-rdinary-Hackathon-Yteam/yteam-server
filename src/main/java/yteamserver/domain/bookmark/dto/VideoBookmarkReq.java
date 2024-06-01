c8df50c316e804df04d1c4380b81746ec1a008d8
OSSU <o.ss.u2056@gmail.com>
2024-06-02 00:06:27 +0900

    feat: 영상 북마크, 인기 셀러 리스트 dto에 스웨거 추가

--- src/main/java/yteamserver/domain/bookmark/dto/VideoBookmarkReq.java
+++ src/main/java/yteamserver/domain/bookmark/dto/VideoBookmarkReq.java
@@ -1,11 +1,18 @@
 package yteamserver.domain.bookmark.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 public class VideoBookmarkReq {
+    @Schema(description = "사용자 토큰",
+            example = "odkaj183-ga94-9aj3-1d81-djal194ns93")
+    @NotNull
     private String token;
+
+    @Schema(description = "북마크할 비디오 아이디", example = "1")
     private Long videoId;
 
     @Builder
