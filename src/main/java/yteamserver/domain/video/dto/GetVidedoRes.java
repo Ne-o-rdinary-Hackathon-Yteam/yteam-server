e198ff5f96f03e453b1054f82084a989e0eb6345
LeeJaeHyeok97 <hazardous10@naver.com>
2024-06-01 21:29:11 +0900

    feat: 숏폼 10개씩 조회 페이징 구현

--- src/main/java/yteamserver/domain/video/dto/GetVidedoRes.java
+++ src/main/java/yteamserver/domain/video/dto/GetVidedoRes.java
@@ -1,11 +1,20 @@
 package yteamserver.domain.video.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
+@AllArgsConstructor
 @NoArgsConstructor
 public class GetVidedoRes {
+    private Long id;
+    private String userName;
+    private Long storeId;
+    private String title;
+    private String ThumbnailUrl;
+    private Integer viewCount;
+    private String videoUrl;
 }
