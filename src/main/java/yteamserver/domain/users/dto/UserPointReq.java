e979a1f2e464575395196d03e1faac55c36fbd21
OSSU <o.ss.u2056@gmail.com>
2024-06-02 00:21:37 +0900

    feat(#31): 유저 포인트 사용 및 적립 dto 스웨거 추가

--- src/main/java/yteamserver/domain/users/dto/UserPointReq.java
+++ src/main/java/yteamserver/domain/users/dto/UserPointReq.java
@@ -1,9 +1,13 @@
 package yteamserver.domain.users.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 
 @Getter
 public class UserPointReq {
+    @Schema(description = "사용자 토큰",
+            example = "odkaj183-ga94-9aj3-1d81-djal194ns93")
     private String token;
+    @Schema(description = "포인트", example = "100 // 100포인트 증가시 +100, 감소시 -100")
     private Integer points;
 }
